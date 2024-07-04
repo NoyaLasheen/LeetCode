c550f3eb1816645716e17890e58f842c0d966314
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2024-07-04 16:22:12 +0530

    Time: 29 ms (19.48%), Space: 11.3 MB (21.12%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,15 +3,12 @@ public:
     int maxVowels(string s, int k) {
         int count = 0, maxCount;
         
-        string str = "aeiou";
-        
+        string str = "aeiou";        
         
         for (int i = 0; i < k; i++) {
             if (str.find(s[i]) != string::npos) {
                 count++;
-            }
-            
-            
+            }          
         }
         maxCount = count;
         
@@ -21,8 +18,7 @@ public:
             }
             if (str.find(s[i]) != string::npos) {
                 count++;
-            }
-                    
+            }                    
                         
             maxCount = max(count, maxCount);
         }
