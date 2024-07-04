339fff47e87a7af7178e52056573e4228a59bc1f
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2024-07-04 16:18:09 +0530

    Time: 31 ms (17.23%), Space: 11.4 MB (14.34%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,22 +3,38 @@ public:
     int maxVowels(string s, int k) {
         int count = 0, maxCount;
         
-        string str = "aeiouAEIOU";
+        // string str = "aeiouAEIOU";
+        unordered_set<char> vowelSet;
+        vowelSet.insert('a'); vowelSet.insert('e'); vowelSet.insert('i'); 
+        vowelSet.insert('o'); vowelSet.insert('u');
         
         for (int i = 0; i < k; i++) {
-            if(str.find(s[i]) != string::npos) {
+            // if (str.find(s[i]) != string::npos) {
+            //     count++;
+            // }
+            
+            if (vowelSet.find(s[i]) != vowelSet.end()) {
                 count++;
             }
         }
         maxCount = count;
         
         for (int i = k; i < s.length(); i++) {
-            if(str.find(s[i - k]) != string::npos) {
+            // if (str.find(s[i - k]) != string::npos) {
+            //     count--;
+            // }
+            // if (str.find(s[i]) != string::npos) {
+            //     count++;
+            // }
+            
+            if (vowelSet.find(s[i - k]) != vowelSet.end()) {
                 count--;
             }
-            if (str.find(s[i]) != string::npos) {
+            
+            if (vowelSet.find(s[i]) != vowelSet.end()) {
                 count++;
             }
+            
             
             maxCount = max(count, maxCount);
         }
