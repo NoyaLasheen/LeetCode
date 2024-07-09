cb686c8fa19d19e1f6401de9179da77dbf1239b3
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2024-07-09 09:57:39 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (10.93%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr_alt = 0, max_alt = 0;
+        
+        for (int i = 0; i < gain.size(); i++) {
+            curr_alt += gain[i];
+            max_alt = max(max_alt, curr_alt);
+        }
+        
+        return max_alt;
+    }
+};
