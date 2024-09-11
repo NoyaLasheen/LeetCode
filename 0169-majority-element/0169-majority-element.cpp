673438f413250e97022887dbf851a56dba1c16ca
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2024-09-11 13:43:40 +0530

    Time: 25 ms (10.94%), Space: 28.2 MB (6.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,13 +3,13 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map <int, int> m;
         
-        for (int i : nums) 
+        for (int i : nums) {
             m[i]++;
+            if (m[i] > nums.size() / 2) return i;
+        } 
+            
         
-        for (auto &pair : m) {
-            if (pair.second > nums.size() / 2) return pair.first;
-        }
-        
+       
         return -1;
     }
 };
