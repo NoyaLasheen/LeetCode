a7feb2d6c049b26dc54a40dafa9b9900627a387d
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2024-09-19 20:15:39 +0530

    Time: 19 ms (35.63%), Space: 21.7 MB (99.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int ind = 0;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size() - i - 1; j++) {
-                if (nums[j] == 0) {
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = 0;
-                }
+            if (nums[i] != 0) {
+                nums[ind++] = nums[i];
             }
+        }
+        while (ind < nums.size()) {
+            nums[ind++] = 0;
         }
     }
 };
