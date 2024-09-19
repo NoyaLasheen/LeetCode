401c5b3e61cc9073600ec513f1cded3efb46ed05
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2024-09-19 20:22:08 +0530

    Time: 7 ms (76.50%), Space: 8.5 MB (99.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,11 +2,29 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        map<char, int> map_s, map_t;
+           if (s.length() != t.length()) return false;
+//         map<char, int> mp;
         
-        for (char ch : s) map_s[ch]++;
-        for (char ch : t) map_t[ch]++;
+//         for (int i = 0; i < s.length(); i++) {
+//             mp[s[i]]++;
+//             mp[t[i]]--;
+//         }
+//         for (auto &pair : mp) {
+//             if (pair.second != 0) return false;
+//         }
+//         return true;
         
-        return map_s == map_t;
+        int arr[52];
+        
+        for (int i = 0; i < s.length(); i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }
+        
+        for (int i : arr) {
+            if (i != 0) return false; 
+            // cout << i << " ";
+        }
+        return true;
     }
 };
