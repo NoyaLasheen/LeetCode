657325750467f36b531738a196bca6943907338b
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2025-01-03 22:17:30 +0530

    Time: 0 ms (100%), Space: 12.2 MB (13.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,12 +3,13 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string curr = strs[0];
         for (string str : strs) {
-            for (int i = 0; i < str.length() && i < curr.length(); i++) {
-                if (curr[i] != str[i]) {
-                    curr = curr.substr(0, i);
-                    break;
-                }
+            int i = 0;
+            while (i < str.length() && i < curr.length() && curr[i] == str[i]) {
+                i++;
             }
+            curr = curr.substr(0, i);
+            if (curr == "") break;
+            
         }
         return curr;
     }
