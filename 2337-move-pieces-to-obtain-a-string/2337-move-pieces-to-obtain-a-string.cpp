eb8802e47abde84c7cd04bf41dabe668c079efb3
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2025-01-16 15:44:27 +0530

    Time: 7 ms (78.4%), Space: 21.9 MB (63.23%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -1,37 +1,29 @@
 class Solution {
-    
-    void skipSpaces(string& s,int& n,int& pos){
-        while(pos<n and s[pos]=='_')
-            pos++;
-    }
-    
 public:
     bool canChange(string start, string target) {
-        int n=start.size();
-
-        int first=0,second=0;
-        int limit_idx = -1;
-        while(first<n){
-            
-            skipSpaces(start,n,first);
-            skipSpaces(target,n,second);
-            
-            if(first==n and second==n)          return true;
-            if(start[first]!=target[second])    return false;
-
-            if(start[first]=='L' and (second<=limit_idx or second>first))
-                return false;
-            else if(start[first]=='R' and first>second)
-                return false;
-    
-            limit_idx = second;
-            first++;
-            second++;
+        int i = 0, j = 0;
+        int n = start.length();
+        while (i < n && j < n) {
+            while (start[i] == '_') i++;
+            while (target[j] == '_') j++;
+            if (start[i] != target[j]) return false;
+            if (start[i] == 'R') {
+                if (j < i) return false;
+                else i++, j++;
+            } else if (start[i] == 'L') {
+                if (j > i) return false;
+                else i++, j++;
+            }
         }
-        
-        skipSpaces(start,n,first);
-        skipSpaces(target,n,second);
 
-        return first==n && second==n;
+        while (i < n) {
+            if (start[i] != '_') return false;
+            i++;
+        }
+        while (j < n) {
+            if (target[j] != '_') return false;
+            j++;
+        }
+        return true;
     }
 };
