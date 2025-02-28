2635771f6e407be192e951de3a36343caf5835db
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2025-02-28 21:51:59 +0530

    Time: 1 ms (66.93%) | Memory: 14.2 MB (55.29%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void findComb(vector<int>& candidates, vector<int>& curr, int idx,
+                  int target, vector<vector<int>>& ans) {
+        if (idx == candidates.size()) {
+            if (target == 0)
+                ans.push_back(curr);
+            return;
+        }
+
+        if (candidates[idx] <= target) {
+            curr.push_back(candidates[idx]);
+            findComb(candidates, curr, idx, target - candidates[idx], ans);
+            curr.pop_back();
+        }
+        findComb(candidates, curr, idx + 1, target, ans);
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> curr;
+        vector<vector<int>> ans;
+        findComb(candidates, curr, 0, target, ans);
+        return ans;
+    }
+};
