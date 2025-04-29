dec4a7b06774a677c26f76bc99f0864490aff9d3
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2025-04-29 21:24:28 +0530

    Time: 0 ms (100%), Space: 79.1 MB (95.07%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<int> leftProf(n, 0), rightProf(n, 0);
-        int maxProf = 0;
-
-        int least = prices[0], most = prices[n - 1];
-        for (int i = 1; i < n; i++) {
-            least = min(least, prices[i]);
-            leftProf[i] = max(leftProf[i - 1], prices[i] - least);
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            most = max(most, prices[i]);
-            rightProf[i] = max(rightProf[i + 1], most - prices[i]);
-        }
+        int cost1 = INT_MAX, cost2 = INT_MAX;
+        int profit1 = 0, profit2 = 0;
 
         for (int i = 0; i < n; i++) {
-            maxProf = max(maxProf, leftProf[i] + rightProf[i]);
+            cost1 = min(cost1, prices[i]);
+            profit1 = max(profit1, prices[i] - cost1);
+            cost2 = min(cost2, prices[i] - profit1);
+            profit2 = max(profit2, prices[i] - cost2);
         }
-        return maxProf;
+
+        return profit2;
     }
 };
