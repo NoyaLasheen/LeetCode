5b189a4cf7953d5fd88fd6de099708b3cd938e97
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2026-02-22 12:44:22 +0530

    Time: 0 ms (100%), Space: 97.4 MB (59.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit = 0;
-        
-        int buy = prices[0];
-        
+        int minCost = prices[0];
+        int maxProf = 0;
+
         for (int i = 1; i < prices.size(); i++) {
-            
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else {
-                maxProfit = max(maxProfit, prices[i] - buy);
-            }
-            
+            maxProf = max (maxProf, prices[i] - minCost);
+            minCost = min(minCost, prices[i]);
         }
-        
-        return maxProfit;
+
+        return maxProf;
     }
 };
